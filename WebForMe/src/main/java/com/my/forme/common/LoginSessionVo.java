7cc478ab7d5d68ab835b3975401095f73f2fec15
DanielLee <edaniel8912@gmail.com>
2021-01-03 23:57:13 +0900

    로그인세션 변수 수정

--- WebForMe/src/main/java/com/my/forme/common/LoginSessionVo.java
+++ WebForMe/src/main/java/com/my/forme/common/LoginSessionVo.java
@@ -1,80 +1,64 @@
 package com.my.forme.common;
 
+import java.util.Date;
+
 public class LoginSessionVo {
 	
-	private Integer usrNo;
-	private String usrId;
-	private String usrNm;
-	private Integer erpId;
-	private String bmd; // 생년월일
+	private Integer uIdx;
+	private String userEmail;
+	private String userPw;
+	private String userNm;
+	private Date cdate;
+	private String useYn; // 생년월일
 	
 	public LoginSessionVo() {}
-	
-	/*
-	public LoginSessionVo( CustomUserDetails details ) {
-		this.usrNo = details.getEsUsrNo();
-		this.usrId = details.getUsrId();
-		this.usrNm = details.getUsrNm();
-		this.erpId = details.getErpId();
-		this.bmd = details.getBmd();
-	}*/
-	
-	/**
-	    @return Returns the usrNo.
-	 */
-	public Integer getUsrNo() {
-		return usrNo;
-	}
-	/**
-	    @param usrNo The usrNo to set.
-	 */
-	public void setUsrNo( Integer usrNo ) {
-		this.usrNo = usrNo;
-	}
-	/**
-	    @return Returns the usrId.
-	 */
-	public String getUsrId() {
-		return usrId;
-	}
-	/**
-	    @param usrId The usrId to set.
-	 */
-	public void setUsrId( String usrId ) {
-		this.usrId = usrId;
-	}
-	/**
-	    @return Returns the usrNm.
-	 */
-	public String getUsrNm() {
-		return usrNm;
-	}
-	/**
-	    @param usrNm The usrNm to set.
-	 */
-	public void setUsrNm( String usrNm ) {
-		this.usrNm = usrNm;
-	}
-	/**
-	    @return Returns the erpId.
-	 */
-	public Integer getErpId() {
-		return erpId;
-	}
-	/**
-	    @param erpId The erpId to set.
-	 */
-	public void setErpId( Integer erpId ) {
-		this.erpId = erpId;
+
+	public Integer getuIdx() {
+		return uIdx;
 	}
 
-	public String getBmd() {
-		return bmd;
+	public void setuIdx(Integer uIdx) {
+		this.uIdx = uIdx;
 	}
 
-	public void setBmd(String bmd) {
-		this.bmd = bmd;
+	public String getUserEmail() {
+		return userEmail;
 	}
-	
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserPw() {
+		return userPw;
+	}
+
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+
+	public String getUserNm() {
+		return userNm;
+	}
+
+	public void setUserNm(String userNm) {
+		this.userNm = userNm;
+	}
+
+	public Date getCdate() {
+		return cdate;
+	}
+
+	public void setCdate(Date cdate) {
+		this.cdate = cdate;
+	}
+
+	public String getUseYn() {
+		return useYn;
+	}
+
+	public void setUseYn(String useYn) {
+		this.useYn = useYn;
+	}
 	
 }
